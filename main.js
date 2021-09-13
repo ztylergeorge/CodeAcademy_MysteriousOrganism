f565409286ea6b3d7da8eaf0ea9e15c3e7ffeb8b
ztylergeorge <ztylergeorge@gmail.com>
2021-09-13 12:25:04 -0400

    creation of object and mutate function

--- main.js
+++ main.js
@@ -13,6 +13,37 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//returns a pAequor object from a factory function
+const pAequorFactory = (specimenID, dna) => {
+  return ({
+    specimenID, 
+    dna, 
+
+    //change one of the sequences of DNA
+    mutate () {
+
+      //get index for change and new sequence value
+      let dnaIndex = Math.floor(Math.random() * this.dna.length);
+      let newSequence = returnRandBase();
+
+      //if new value is equal to current index, change it to something else
+      while (newSequence === this.dna[dnaIndex]) {
+        newSequence = returnRandBase();
+      }
+
+      //change the sequence
+      this.dna[dnaIndex] = newSequence;
+
+      return this.dna;
+    }
+  });
+}
+
+let spec1 = pAequorFactory(1, mockUpStrand());
+console.log(spec1.dna);
+spec1.mutate();
+console.log(spec1.dna);
+
 
 
 
